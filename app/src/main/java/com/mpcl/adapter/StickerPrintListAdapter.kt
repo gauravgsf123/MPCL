f4ea1781cf4668629da578e3a87f631f7ea06ac0
Gaurav Kumar <gaurav.kumar@quytech.com>
2022-03-04 09:11:21 +0530

    push code

--- app/src/main/java/com/mpcl/adapter/StickerPrintListAdapter.kt
+++ app/src/main/java/com/mpcl/adapter/StickerPrintListAdapter.kt
@@ -1,18 +1,24 @@
 package com.mpcl.adapter
 
 import android.annotation.SuppressLint
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.mpcl.R
+import com.mpcl.custom.BoldTextView
 import com.mpcl.custom.RegularTextView
 import com.mpcl.model.StickerDataResponseModel
 
 class StickerPrintListAdapter: RecyclerView.Adapter<StickerPrintListAdapter.MyViewHolder>() {
     var itemClick: ((StickerDataResponseModel) -> Unit)? = null
     private var stockList = listOf<StickerDataResponseModel>()
-    fun setItems(stockList: List<StickerDataResponseModel>) {
+    private lateinit var context :Context
+    fun setItems(stockList: List<StickerDataResponseModel>,context:Context) {
+        this.context = context
         this.stockList = stockList
         notifyDataSetChanged()
     }
@@ -29,7 +35,7 @@ class StickerPrintListAdapter: RecyclerView.Adapter<StickerPrintListAdapter.MyVi
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
         val viewHolder = holder
-        viewHolder.bindView(stockList.get(position),position)
+        viewHolder.bindView(stockList.get(position),position,context)
     }
 
     override fun getItemCount(): Int {
@@ -38,18 +44,20 @@ class StickerPrintListAdapter: RecyclerView.Adapter<StickerPrintListAdapter.MyVi
 
     class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         var itemClick: ((StickerDataResponseModel) -> Unit)? = null
-        private lateinit var cNoteNumber: RegularTextView
-        private lateinit var cartonNo: RegularTextView
+        private lateinit var cNoteNumber: BoldTextView
+        private lateinit var cartonNo: BoldTextView
+        private lateinit var constLayout: ConstraintLayout
         @SuppressLint("ResourceAsColor")
-        fun bindView(model: StickerDataResponseModel, i:Int) {
+        fun bindView(model: StickerDataResponseModel, i: Int, context: Context) {
              cNoteNumber = itemView.findViewById(R.id.tv_c_note_number)
             cartonNo = itemView.findViewById(R.id.tv_carton_no)
+            constLayout = itemView.findViewById(R.id.const_top)
             cNoteNumber.text = model.CNoteNo
             cartonNo.text = model.BarCodeNo
             if(model.printDone){
-                cNoteNumber.setTextColor(R.color.green)
+                constLayout.setBackgroundColor(ContextCompat.getColor(context, R.color.green_light))
             }else{
-                cNoteNumber.setTextColor(R.color.red)
+                constLayout.setBackgroundColor(ContextCompat.getColor(context, R.color.red_light))
             }
 
             itemView.setOnClickListener{
